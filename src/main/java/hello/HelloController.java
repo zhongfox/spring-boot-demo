a599983c62f00b01a09defa408b615eff849b131
zhongfox <foxzhong@tencent.com>
2018-08-28 21:54:39 +0800

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Hello from Spring Boot!";
+        return "Greetings from Spring Boot!";
     }
 
 }
