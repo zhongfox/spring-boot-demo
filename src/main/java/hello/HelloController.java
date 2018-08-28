61dca7e4ea32158b2d782d0aeea4181359653e1b
zhongfox <foxzhong@tencent.com>
2018-08-28 22:12:36 +0800

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
-
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
