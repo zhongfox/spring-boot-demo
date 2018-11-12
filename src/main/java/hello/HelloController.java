43f1ea6c998132d824a4ec4650d282a441103077
fox <foxzhong@tencent.com>
2018-11-12 11:02:10 +0800

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,8 @@ public class HelloController {
     @RequestMapping("/")
     public String index() {
         System.out.println("hello tencenthub user");
+        
          // unit test will check: the return string must contains "Spring Boot"
-        return "<body style='background-color:green;' ><h1>Hi Spring Boot!<h1></body>";
+        return "<body style='background-color:red;' ><h1>Hi Spring Boot!<h1></body>";
     }
 }
