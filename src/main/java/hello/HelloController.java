de69f482b2d1dc2a1381598286cd47695ef410db
fox <foxzhong@tencent.com>
2018-11-15 14:24:17 +0800

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -11,6 +11,6 @@ public class HelloController {
         
          // unit test will check: the return string must contains "Spring Boot"
         
-        return "<body style='background-color:red;' ><h1>Hi Spring Boot!...<h1></body>";
+        return "<body style='background-color:blue;' ><h1>Hi Spring Boot!...<h1></body>";
     }
 }
